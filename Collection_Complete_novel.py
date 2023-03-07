a54cfdebeb68891fc76740d91b2f5df1fdfb5345
all sky <43485379+allrobot@users.noreply.github.com>
2023-03-07 09:26:45 +0800

    Update Collection_Complete_novel.py
    更改asyncio并发，下载更快了

--- Collection_Complete_novel.py
+++ Collection_Complete_novel.py
@@ -1,37 +1,31 @@
 # -*- coding: utf-8 -*-
-import os.path
+import asyncio
+import datetime
+import os
 import re
+import socket
 import sys
 import time
 import traceback
+from concurrent.futures import ThreadPoolExecutor
+
+import aiofiles
+from aiohttp.resolver import AsyncResolver
+import aiohttp
 import cchardet
 import requests
+
 from bs4 import BeautifulSoup
 
-encoding = ''
-
-sensitive_words = []
 with open('敏感词.TXT', 'r', encoding='utf-8') as f:
-    [sensitive_words.append(word[:-1]) for word in f.readlines()]
-
-index = 'https://www.cool18.com/bbs4/'
-
-
-# 含有敏感词，返回False
-def check_sensitive(input):
-    if re.search('(.*?)(作者|送达者)', input):
-        for sensitive_word in sensitive_words:
-            if sensitive_word in re.search('(.*?)作者', input).group(1):
-                return False
-    else:
-        for sensitive_word in sensitive_words:
-            if sensitive_word in input:
-                return False
-    return True
+    sensitive_words = '('
+    for word in f.readlines():
+        sensitive_words = sensitive_words + word[:-1] + '|'
+sensitive_words = sensitive_words[:-1] + ')'
 
 
 # 去除非法字符
-def rmsc(string):
+async def del_illegal_words(string):
     string = string.replace('\\', ' - ')
     string = string.replace('/', ' - ')
     string = string.replace(':', '：')
@@ -42,14 +36,14 @@ def rmsc(string):
     string = string.replace('>', '】')
     string = string.replace('|', '-')
     string = string.replace('\t', ' ')
-    string = string.replace('\r', ' ')
-    string = string.replace('\n', ' ')
+    string = string.replace('\r', '')
+    string = string.replace('\n', '')
     string = string.strip()
     return string
 
 
 # 正文去掉网页元素
-def remove_html(text):
+async def remove_html(text):
     text = re.sub(r'<font(.*?)font>', '', text)
     text = re.sub(r'<br/>', '\n', text)
     text = re.sub(r'<p>', '\n', text)
@@ -60,158 +54,218 @@ def remove_html(text):
     return text
 
 
-# 重复文件返回文件副本名
-def return_copy_name(name):
-    if os.path.exists(name):
-        num = 0
-        while os.path.exists(name):
-            copy_num = re.search('(.*?)（(.*?)）.txt', name)
-            if copy_num:
-                if copy_num.group(2).isnumeric():
-                    num += 1
-                    name = copy_num.group(1) + ' - 副本（' + str(num) + '）.txt'
-                else:
-                    name = name[:-4] + ' - 副本（' + str(num) + '）.txt'
-                    num += 1
-            else:
-                name = name[:-4] + ' - 副本（' + str(num) + '）.txt'
-                num += 1
-    return name
+# 含有敏感词，返回False
+async def check_sensitive(input):
+    if re.search(f'(.*?){sensitive_words}(.*?)(作者|送达者|$)', input):
+        return False
+    else:
+        return True
 
 
-# 返回请求的网页元素
-def request_url_source_code(url):
-    HEADER = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0)'
-                      ' Gecko/20100101 Firefox/106.0',
-    }
+# 返回文件副本名
+async def copy_name(filename):
     num = 0
-    while num < 30:
+    while os.path.exists(filename):
+        copy_num = re.search('(.*?)（(.*?)）.txt', filename)
         num += 1
+        if copy_num:
+            if copy_num.group(2).isnumeric():
+                filename = copy_num.group(1) + ' - 副本（' + str(num) + '）.txt'
+            else:
+                filename = filename[:-4] + ' - 副本（' + str(num) + '）.txt'
+        else:
+            filename = filename[:-4] + ' - 副本（' + str(num) + '）.txt'
+    return filename
+
+
+# 用于保存文章，如果文件已存在，判断文件内容是否一致，不一致则重命名文件保存内容
+async def write_file(filename, content):
+    content = await remove_html(content)
+    if os.path.exists(filename):
+        async with aiofiles.open(filename, 'rb') as f:
+            encoding = cchardet.detect(await f.read())['encoding']
+        async with aiofiles.open(filename, 'r', encoding=encoding, errors='ignore') as f:
+            if await f.read() != content:
+                newname = await copy_name(filename)
+                # await loop.run_in_executor(None, os.rename, filename, newname)
+                async with aiofiles.open(newname, 'w', errors='ignore') as file:
+                    await file.write(content)
+    else:
+        async with aiofiles.open(filename, 'w', errors='ignore') as file:
+            await file.write(content)
+
+
+async def fetch(session, url, semaphore, proxy):
+    error = 0
+    while error < 30:
+        error += 1
         try:
-            r = requests.get(url, headers=HEADER)
-            global encoding
-            encoding = cchardet.detect(r.content)['encoding']
-            return BeautifulSoup(r.text, 'lxml')
+            params = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0)'
+                              ' Gecko/20100101 Firefox/106.0',
+            }
+            async with semaphore:
+                async with session.get(url, params=params, proxy=proxy) as response:
+                    source_code = BeautifulSoup(await response.text(), 'lxml')
+                    # loop.run_in_executor()
+                    # print(f'正在访问{url}中。。。')
+                    await article_save(source_code, session, semaphore, proxy, url)
+                    error = 30
+
         except:
-            if num == 30:
-                traceback.print_exc()
-                print('超过最大重连次数')
-                sys.exit()
-            print(url + ' 获取失败，正在重连...')
-            time.sleep(10 * num)
+            if not error < 30:
+                message = f'【{datetime.datetime.now()}】：无法连接 {url}\n错误日志: {traceback.format_exc()}\n'
+                async with aiofiles.open('../错误日志.txt', 'a+') as file:
+                    await file.write(message)
+                print(message)
 
 
-exclude_links = []
-
-
-# 获取最新的最大文章数量
-def get_max_num():
-    num = request_url_source_code(index)
-    limit_num = num.select_one('#d_list > ul > li > a:nth-child(1)>font[color="grey"]').parent['href']
-    limit_num = int(re.search('tid\=(.*?)$', limit_num).group(1).strip())
-    return limit_num
-
-
-if __name__ == '__main__':
-    max_num = get_max_num()
-
-    num = 1
-    # 一直爬到最新的小说...
-    while num < max_num:
-        Current_url = 'https://www.cool18.com/bbs4/index.php?app=forum&act=threadview&tid=' + str(num)
-        num += 1
-        # 如果网址已经加载过了，后缀加1
-        for exclude_url in exclude_links:
-            if exclude_url == Current_url:
-                Current_url = 'https://www.cool18.com/bbs4/index.php?app=forum&act=threadview&tid=' + str(num)
-                exclude_links.remove(exclude_url)
-                break
+async def get_link_pre(session, url, semaphore, proxy):
+    error = 0
+    while error < 30:
+        error += 1
         try:
-            source_code = request_url_source_code(Current_url)
-            content = source_code.select('td.show_content > pre')
-            follow_up = source_code.select('body > table >  tr > td > ul li')
-            if not source_code.select_one('head>script') and not source_code.select('td.show_content>p'):
-                if content:
-                    first_pre_article_content = remove_html(str(content[0])) + '\n'
-                    article_name = source_code.select('td.show_content>center>font')[0].text
-                    if check_sensitive(article_name):
-                        article_filename = article_name + '.txt'
+            params = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0)'
+                              ' Gecko/20100101 Firefox/106.0',
+            }
+            async with semaphore:
+                async with session.get(url, params=params, proxy=proxy) as response:
+                    source_code = BeautifulSoup(await response.text(), 'lxml')
+                    return source_code
+        except:
+            if not error < 30:
+                message = f'【{datetime.datetime.now()}】：无法连接 {url}\n错误日志: {traceback.format_exc()}\n'
+                async with aiofiles.open('../错误日志.txt', 'a+') as file:
+                    await file.write(message)
+                print(message)
+
+
+async def get_max(session, url, proxy):
+    error = 0
+    while error < 30:
+        error += 1
+        try:
+            params = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0)'
+                              ' Gecko/20100101 Firefox/106.0',
+            }
+            async with session.get(url, params=params, proxy=proxy) as response:
+                source_code = BeautifulSoup(await response.text(), 'lxml')
+                max_num = source_code.select_one('#d_list > ul > li > a:nth-child(1)>font[color="grey"]').parent[
+                    'href']
+                return int(re.search('tid\=(.*?)$', max_num).group(1).strip())
+
+        except:
+            if not error < 30:
+                message = f'【{datetime.datetime.now()}】：无法连接 {url}\n错误日志: {traceback.format_exc()}\n'
+                async with aiofiles.open('../错误日志.txt', 'a+') as file:
+                    await file.write(message)
+                print(message)
+
+
+'''
+提取正文进行一些操作
+提取某元素的正文，查看是否有回帖
+    存在回帖，提取里面的文章
+合并文章并且保存
+'''
+byte = re.compile('\d+')
+del_bracket = re.compile('(.*?)(\(|（)(.*?)(\)|）)(.*?)$')
+end_num = re.compile('tid=(\d+)')
+index = 'https://www.cool18.com/bbs4/'
+
+
+async def article_save(source_code, session, semaphore, proxy, Currect_url):
+    try:
+        content = source_code.select('td.show_content > pre')
+        follow_up = source_code.select('body > table >  tr > td > ul li')
+
+        # 判断网址是否合法的文章
+        if not source_code.select_one('head>script') and not source_code.select('td.show_content>p'):
+            if content:
+                first_pre_article_content = await remove_html(str(content[0])) + '\n'
+                article_name = source_code.select('td.show_content>center>font')[0].text
+                if await check_sensitive(article_name):
+                    article_filename = await del_illegal_words(article_name) + '.txt'
+                    valid = False
+                    if len(first_pre_article_content) > 0:
+                        valid = True
+                    follow_up_links = []  # 回帖列表初始化
+                    if follow_up:  # 存在回帖
+                        for li in follow_up:  # 遍历回帖，记录较多字符的回帖
+                            new_a = li.select_one('a')
+                            # 检查文章大小
+                            if int(byte.search(new_a.next_sibling).group(0)) > 10000:
+                                valid = True
+                                if await check_sensitive(str(new_a.text)):
+                                    new_ = []
+                                    new_.append(new_a.text)
+                                    new_.append(index + new_a['href'])
+                                    try:
+                                        if isinstance(new_[0], str):
+                                            follow_up_links.append(new_)
+                                    except:
+                                        pass
+                    elif len(first_pre_article_content) < 1000:  # 字数少，跳过
                         valid = False
-                        if len(first_pre_article_content) > 0:
-                            valid = True
-                        follow_up_links = []  # 回帖列表初始化
-                        if follow_up:  # 存在回帖
-                            for li in follow_up:  # 遍历回帖，记录较多字符的回帖
-                                new_list = []
-                                new_a = li.select_one('a')
 
-                                # 防止重复添加链接
-                                exist_exclude_url = True
-                                for link in exclude_links:
-                                    if index + new_a['href'] == link:
-                                        # 回帖链接添加到排除列表，下次遍历链接无需再次打开
-                                        exist_exclude_url = False
-                                if exist_exclude_url:
-                                    exclude_links.append(index + new_a['href'])
+                    # 该页面字数较多，存为txt文件
+                    if valid:
+                        # 文章字符串
+                        text = article_name + '\n' + first_pre_article_content + '\n'
+                        if follow_up_links:
+                            follow_up_links = follow_up_links[::-1]
+                            bracket_exist = del_bracket.search(article_filename)
+                            if bracket_exist:
+                                article_filename = bracket_exist.group(1) + bracket_exist.group(5) + '.txt'
+                            # 打开回帖，将文章内容加到列表中
+                            for article_link in follow_up_links:
+                                article_source_code = await asyncio.ensure_future(
+                                    get_link_pre(session, article_link[1], semaphore, proxy))
+                                article_bre = article_source_code.select('td.show_content > pre')
+                                article_text = await remove_html(str(article_bre[0]))
+                                text += '\n' + article_link[0] + '\n\n来源：' + article_link[
+                                    1] + '\n' + article_text + '\n'
 
-                                # 检查文章大小
-                                if int(re.search('\((.*?) bytes\)', new_a.next_sibling).group(1)) > 10000:
-                                    Valid = True
-                                    if check_sensitive(str(new_a.text)):
-                                        new_ = []
-                                        # new_.append(classify)
-                                        new_.append(new_a.text)
-                                        new_.append(index + new_a['href'])
-                                        try:
-                                            if isinstance(new_[0], str):
-                                                follow_up_links.append(new_)
-                                        except:
-                                            pass
-                        elif len(first_pre_article_content) < 1000:  # 字数少，跳过
-                            valid = False
-                        # 该页面字数较多，存为txt文件
-                        if Valid:
-                            if follow_up_links:
-                                follow_up_links = follow_up_links[::-1]
-                                bracket_exist = re.search('(.*?)(\(|（)(.*?)(\)|）)(.*?).txt', article_filename)
-                                if bracket_exist:
-                                    article_filename = bracket_exist.group(1) + bracket_exist.group(5) + '.txt'
-                                # 打开回帖，将回帖内容加到列表中
-                                for article_link in follow_up_links:
-                                    article_source_code = request_url_source_code(article_link[1])
-                                    article_bre = article_source_code.select('td.show_content > pre')
-                                    article_text = remove_html(str(article_bre[0]))
-                                    article_link.append(article_text)
-                            if not os.path.exists('禁忌书屋'):
-                                os.mkdir('禁忌书屋')
-                            filepath = '禁忌书屋/' + rmsc(article_filename)
+                        await write_file(article_filename, text)
 
-                            # 合并文章
-                            text = article_name + '\n' + first_pre_article_content + '\n'
-                            if follow_up_links:
-                                for article in follow_up_links:
-                                    text += '\n' + article[0] + '\n\n来源：' + article[1] + '\n' + article[2] + '\n'
+                        print(f'{Currect_url} 已访问\n已保存 {article_name}\t')
 
-                            # 如果有重复文件，文件大小一致则跳过，否则新建xx（1）.txt，xx（2）.txt之类的文件
-                            if os.path.exists(filepath):
-                                with open(filepath, 'rb') as f:
-                                    exists_encoding = cchardet.detect(f.read())['encoding']
-                                with open(filepath, 'r', encoding=exists_encoding) as f:
-                                    file_len = len(f.read())
-                                if file_len != len(text):
-                                    filepath = '禁忌书屋/' + return_copy_name(rmsc(article_filename))
-                                    with open(filepath, 'w', encoding=encoding, errors='ignore') as f:
-                                        f.write(text)
-                                        print(Current_url, '爬取完毕，还有',
-                                              str(max_num - int(
-                                                  re.search('tid\=(.*?)$', Current_url).group(1).strip())), '页')
+    except:
+        traceback.print_exc()
+        message = f'【{datetime.datetime}】：{traceback.format_exc()}\n' + Currect_url + '\n'
+        async with aiofiles.open('../错误日志.txt', 'a+') as file:
+            await file.write(message)
+        print(Currect_url)
+        # sys.exit()
 
-                            else:
-                                with open(filepath, 'w', encoding=encoding, errors='ignore') as f:
-                                    f.write(text)
-                                print(Current_url, '爬取完毕，还有',
-                                      str(max_num - int(re.search('tid\=(.*?)$', Current_url).group(1).strip())), '页')
-        except:
-            traceback.print_exc()
-            print(Current_url)
+
+async def main():
+    proxy = 'http://127.0.0.1:1162'
+    new_file = await del_illegal_words(f'禁忌书屋 {str(datetime.datetime.now())[:-16]}')
+    if not os.path.exists(new_file):
+        os.mkdir(new_file)
+    os.chdir(new_file)
+    semaphore_num = 50
+    semaphore = asyncio.Semaphore(semaphore_num)
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        # 获取最新的页数
+        max_index = await asyncio.ensure_future(get_max(session, index, semaphore, proxy))
+        index_num = 0
+        while index_num < max_index:
+            if index_num + 600 >= max_index:
+                index_num += (max_index - index_num)
+            else:
+                index_num += 600
+
+            for i in range(index_num - 600, index_num):
+                print(f'正在创建访问列表中...已添加第{i}页', end='\r', flush=True)
+                url = f'https://www.cool18.com/bbs4/index.php?app=forum&act=threadview&tid={i}'
+                tasks.append(asyncio.ensure_future(fetch(session, url, semaphore, proxy)))
+            await asyncio.gather(*tasks)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
